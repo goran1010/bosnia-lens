ba78b19c43b17585e566c13ddaccd85986e2d2d7
Goran Jovic <goran1010jovic@gmail.com>
2025-11-12 19:30:36 +0100

    feat: add layout for PostalCodes component

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,7 +3,11 @@ import { Link } from "react-router-dom";
 export default function Navbar() {
   return (
     <nav>
-      <ul>
+      <ul
+        className="
+        flex w-full p-4 bg-gray-200
+        justify-center gap-8"
+      >
         <li>
           <Link to="/">Home</Link>
         </li>
--- frontend/src/components/PostalCodes.jsx
+++ frontend/src/components/PostalCodes.jsx
@@ -1,3 +1,29 @@
 export default function PostalCodes() {
-  return <main>Postal Codes</main>;
+  return (
+    <main className="flex justify-center">
+      <form action="">
+        <label
+          htmlFor="search-term"
+          className="block text-sm font-medium text-gray-700 mb-1"
+        >
+          Postal Code or Municipality
+        </label>
+        <div className="flex">
+          <input
+            type="text"
+            name="search-term"
+            id="search-term"
+            className="block border border-gray-300 rounded-md p-2"
+          />
+
+          <button
+            type="submit"
+            className="bg-blue-600 text-white p-2 rounded-md hover:bg-blue-700 hover:cursor-pointer active:scale-98"
+          >
+            Search
+          </button>
+        </div>
+      </form>
+    </main>
+  );
 }
