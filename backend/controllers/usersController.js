ccf2a965f06e91116cedc6994e52a3df9cc3048e
Goran Jovic <goran1010jovic@gmail.com>
2025-11-18 14:12:17 +0100

    feat: updated access_tokens to last 15 minutes

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -6,7 +6,9 @@ const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET;
 
 if (!ACCESS_TOKEN_SECRET || !REFRESH_TOKEN_SECRET) {
-  throw new Error("Missing required environment variables: ACCESS_TOKEN_SECRET or REFRESH_TOKEN_SECRET");
+  throw new Error(
+    "Missing required environment variables: ACCESS_TOKEN_SECRET or REFRESH_TOKEN_SECRET",
+  );
 }
 
 export async function signup(req, res) {
@@ -63,7 +65,7 @@ export async function login(req, res) {
     { id: user.id, username: user.username },
     ACCESS_TOKEN_SECRET,
     {
-      expiresIn: "2m",
+      expiresIn: "15m",
     },
   );
 
