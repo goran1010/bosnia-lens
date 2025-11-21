aaf6175ffaa55e6211bbad7430e8e88063adc913
Goran Jovic <goran1010jovic@gmail.com>
2025-11-21 12:20:16 +0100

    test: add test for incorrect access token

--- backend/tests/authRouter.test.js
+++ backend/tests/authRouter.test.js
@@ -18,7 +18,22 @@ describe("GET /check-auth", () => {
     expect(response.body).toEqual(notLoggedInResponse);
   });
 
-  test("responds with status 401 and Need to be logged in if not logged in", async () => {
-    
+  test("responds with status 403 and Expired session token in if not logged in", async () => {
+    const notLoggedInResponse = {
+      errors: [
+        {
+          msg: "Expired session token",
+        },
+      ],
+    };
+    const expiredAccessToken =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImJiZTU4NWUyLTdiOTktNDllMS1iZmE2LWZlODQzY2Y0YWJiNCIsInVzZXJuYW1lIjoibmV3VXNlciIsImlhdCI6MTc2MzY2NTM3NywiZXhwIjoxNzYzNjY2Mjc3fQ.nLmQyXxsvWSR4h6BUOzwy9zPL_JDoEJi57hiwiu5NTc";
+    const response = await request(app)
+      .get("/auth/check-auth")
+      .set("Authorization", `Token ${expiredAccessToken}`);
+
+    expect(response.header["content-type"]).toMatch(/json/);
+    expect(response.status).toBe(403);
+    expect(response.body).toEqual(notLoggedInResponse);
   });
 });
