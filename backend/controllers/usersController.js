c2d58a1e2ba0d7d07db2cf53a05c5b1fa1ad8095
Goran Jovic <goran1010jovic@gmail.com>
2025-11-23 15:17:15 +0100

    feat: create a nicer looking confirm email html page

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -98,7 +98,16 @@ export async function confirmEmail(req, res) {
       where: { id: tokenRecord.id },
     });
 
-    res.json({ message: "Email confirmed successfully" });
+    res.send(`    <html>
+      <head>
+        <title>Email Confirmed</title>
+      </head>
+      <body>
+        <h1>Your email has been confirmed!</h1>
+        <p>You can now close this window and <a href="${process.env.URL}/login">log in</a> to your account.</p>
+      </body>
+    </html>
+   `);
   } catch (err) {
     // eslint-disable-next-line no-console
     console.error(err);
