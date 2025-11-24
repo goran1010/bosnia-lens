dff37292ab7c8a49ae248f353491bceb46bae6eb
Goran Jovic <goran1010jovic@gmail.com>
2025-11-24 21:23:09 +0100

    feat: add messages UI to postalcodes

--- frontend/src/components/PostalCodes.jsx
+++ frontend/src/components/PostalCodes.jsx
@@ -1,10 +1,13 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useContext } from "react";
 import checkPostalCodesValidity from "../utils/formValidation/checkPostalCodesValidity";
 import Spinner from "@goran1010/spinner";
+import UserDataContext from "../utils/UserDataContext";
 
 const URL = import.meta.env.VITE_BACKEND_URL;
 
 export default function PostalCodes() {
+  const { message, setMessage } = useContext(UserDataContext);
+
   const [searchTerm, setSearchTerm] = useState("");
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -25,10 +28,12 @@ export default function PostalCodes() {
       const result = await response.json();
 
       if (!response.ok) {
+        setMessage([result.error, result.details]);
         return console.warn(result.error, result.details);
       }
       setSearchResult(result.data);
     } catch (err) {
+      setMessage([err]);
       console.error(err);
     } finally {
       setLoading(false);
@@ -46,10 +51,12 @@ export default function PostalCodes() {
       const result = await response.json();
 
       if (!response.ok) {
+        setMessage([result.error, result.details]);
         return console.warn(result.error, result.details);
       }
       setSearchResult(result.data);
     } catch (err) {
+      setMessage([err]);
       console.error(err);
     } finally {
       setLoading(false);
@@ -69,6 +76,7 @@ export default function PostalCodes() {
           >
             Postal Code or Municipality
           </label>
+
           <div className="flex gap-2">
             <input
               ref={searchInput}
@@ -100,9 +108,41 @@ export default function PostalCodes() {
         </form>
       </section>
       <div>{loading && <Spinner />}</div>
+      {message[0] && (
+        <div className="relative">
+          <div className=" p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 border border-blue-300">
+            <div className="mb-1 last:mb-0">
+              <h2 className="text-2xl">{message[0]}</h2>
+              {message[1] && message?.length > 0 && (
+                <p>
+                  {message[1].map((element) => {
+                    return <li>{element.msg}</li>;
+                  })}
+                </p>
+              )}
+            </div>
+          </div>
+          <button
+            onClick={() => setMessage([])}
+            className="absolute top-1 right-1 text-blue-800 hover:text-blue-900"
+            aria-label="Close message"
+          >
+            <svg
+              className="cursor-pointer w-4 h-4"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+            >
+              <path
+                fillRule="evenodd"
+                d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              />
+            </svg>
+          </button>
+        </div>
+      )}
       <section className="flex flex-col justify-center items-center">
-        <h2>Postal codes & municipalities</h2>
-        <ul className="flex flex-col border">
+        <ul className="flex flex-col">
           {searchResult &&
             searchResult.map((result) => {
               return (
