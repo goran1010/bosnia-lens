1271579422db25137caac1d3266ecb3b5a2608f4
Goran Jovic <goran1010jovic@gmail.com>
2025-11-24 14:08:55 +0100

    feat: reword the error page

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function ErrorPage() {
   return (
     <div className="error-page">
-      <p>This is a custom 404 error page.</p>
+      <p>There is nothing here, sorry.</p>
       <p>
         <Link to="/">Go Home</Link>
       </p>
