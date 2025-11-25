b6294e1edcba9e21c44f2dc35e41e05d1f33e435
Goran Jovic <goran1010jovic@gmail.com>
2025-11-25 13:18:00 +0100

    refactor: push message element in SignUp to MessageCard

--- frontend/src/components/SignUp.jsx
+++ frontend/src/components/SignUp.jsx
@@ -5,6 +5,7 @@ import checkFormValidityClick from "../utils/formValidation/checkFormValidityCli
 import UserDataContext from "../utils/UserDataContext";
 import Spinner from "@goran1010/spinner";
 import useSignUpForm from "../utils/handleForm/handleSignUpForm";
+import MessageCard from "./MessageCard.jsx";
 
 export default function SignUp() {
   const [loading, setLoading] = useState(false);
@@ -41,38 +42,9 @@ export default function SignUp() {
     <div className=" min-h-full flex items-center justify-center bg-gray-50 ">
       <div className=" relative w-full max-w-md p-6 flex flex-col gap-3">
         {message[0] && (
-          <div className="relative">
-            <div className=" p-4 mb-4 text-sm text-blue-800 rounded-lg bg-blue-50 border border-blue-300">
-              <div className="mb-1 last:mb-0">
-                <h2 className="text-2xl">{message[0]}</h2>
-                {message[1] && (
-                  <p>
-                    {message[1].map((element) => {
-                      return <li>{element.msg}</li>;
-                    })}
-                  </p>
-                )}
-              </div>
-            </div>
-            <button
-              onClick={() => setMessage([])}
-              className="absolute top-1 right-1 text-blue-800 hover:text-blue-900"
-              aria-label="Close message"
-            >
-              <svg
-                className="cursor-pointer w-4 h-4"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </button>
-          </div>
+          <MessageCard message={message} setMessage={setMessage} />
         )}
+
         <div>
           <h1 className="text-5xl mb-8 text-center font-bold text-gray-900">
             Create your account
