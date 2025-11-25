b2c8f893cebca8d36c02fb0a537df8e10857c692
Goran Jovic <goran1010jovic@gmail.com>
2025-11-25 17:01:44 +0100

    refactor: move all postal codes in PostalCodes to GetAllPostalCodes

--- frontend/src/components/PostalCodes.jsx
+++ frontend/src/components/PostalCodes.jsx
@@ -1,38 +1,12 @@
-import { useState, useContext } from "react";
+import { useState } from "react";
 import Spinner from "@goran1010/spinner";
-import UserDataContext from "../utils/UserDataContext";
 import MessageCard from "./MessageCard";
 import SearchPostalCode from "./SearchPostalCode";
-
-const URL = import.meta.env.VITE_BACKEND_URL;
+import GetAllPostalCodes from "./GetAllPostalCodes";
 
 export default function PostalCodes() {
-  const { setMessage } = useContext(UserDataContext);
   const [searchResult, setSearchResult] = useState([]);
   const [loading, setLoading] = useState(false);
-
-  async function handleGetAll(e) {
-    try {
-      setLoading(true);
-      e.preventDefault();
-
-      const response = await fetch(`${URL}/api/v1/postal-codes`, {
-        mode: "cors",
-      });
-      const result = await response.json();
-
-      if (!response.ok) {
-        setMessage([result.error, result.details]);
-        return console.warn(result.error, result.details);
-      }
-      setSearchResult(result.data);
-    } catch (err) {
-      setMessage([err]);
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  }
 
   return (
     <>
@@ -41,16 +15,12 @@ export default function PostalCodes() {
           setSearchResult={setSearchResult}
           setLoading={setLoading}
         />
-        <form onSubmit={handleGetAll}>
-          <button
-            type="submit"
-            className="bg-yellow-600 text-white p-2 rounded-md hover:bg-yellow-700 hover:cursor-pointer active:scale-98"
-          >
-            Get All Postal Codes and Municipalities
-          </button>
-        </form>
+        <GetAllPostalCodes
+          setSearchResult={setSearchResult}
+          setLoading={setLoading}
+        />
       </section>
-      <div>{loading && <Spinner />}</div>
+      {loading && <Spinner />}
       <MessageCard />
       <section className="flex flex-col justify-center items-center">
         <ul className="flex flex-col">
