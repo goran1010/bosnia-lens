c93c601738b9c10d47404a0b70756413f6ae1702
Goran Jovic <goran1010jovic@gmail.com>
2025-12-01 13:10:26 +0100

    feat: move controllers to their own modules

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,3 +1,59 @@
+import axios from "axios";
+import prisma from "../db/prisma.js";
+
+const gitHubClientId = process.env.CLIENT_ID;
+const gitHubClientSecret = process.env.CLIENT_SECRET;
+
 export function me(req, res) {
   res.json({ data: { accessToken: req.token } });
+}
+
+export async function githubLogin(req, res) {
+  const params = {
+    scope: "read:user",
+    client_id: gitHubClientId,
+  };
+
+  const urlEncodedParams = new URLSearchParams(params).toString();
+  res.redirect(`https://github.com/login/oauth/authorize?${urlEncodedParams}`);
+}
+
+export async function githubCallback(req, res) {
+  const { code } = req.query;
+
+  const body = {
+    client_id: gitHubClientId,
+    client_secret: gitHubClientSecret,
+    code,
+  };
+
+  const options = { headers: { accept: "application/json" } };
+
+  const response = await axios.post(
+    "https://github.com/login/oauth/access_token",
+    body,
+    options,
+  );
+
+  const accessToken = response.data.access_token;
+
+  const userData = await axios.get(`https://api.github.com/user`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+  });
+
+  const { email, login } = userData.data;
+
+  let userExists = await prisma.user.findUnique({ where: { email } });
+
+  if (userExists && !userExists.isEmailConfirmed) {
+    userExists = await prisma.user.update({
+      where: { email: userExists.email },
+      data: { isEmailConfirmed: true },
+    });
+  }
+
+  res.status(201).json({
+    data: { user: { email, username: login } },
+    accessToken: "some-token",
+  });
 }
--- backend/routes/authRouter.js
+++ backend/routes/authRouter.js
@@ -2,40 +2,11 @@ import { Router } from "express";
 const authRouter = Router();
 import * as authController from "../controllers/authController.js";
 import isAuthenticated from "../auth/isAuthenticated.js";
-import axios from "axios";
 
 authRouter.get("/me", isAuthenticated, authController.me);
 
-authRouter.get("/", (req, res) => {
-  const params = {
-    scope: "read:user",
-    client_id: process.env.CLIENT_ID,
-  };
+authRouter.get("/", authController.githubLogin);
 
-  const urlEncodedParams = new URLSearchParams(params).toString();
-  res.redirect(`https://github.com/login/oauth/authorize?${urlEncodedParams}`);
-});
-
-authRouter.get("/github-callback", async (req, res) => {
-  const { code } = req.query;
-
-  const body = {
-    client_id: process.env.CLIENT_ID,
-    client_secret: process.env.CLIENT_SECRET,
-    code,
-  };
-
-  const options = { headers: { accept: "application/json" } };
-
-  const response = await axios.post(
-    "https://github.com/login/oauth/access_token",
-    body,
-    options,
-  );
-
-  const token = response.data.access_token;
-
-  res.json(`token = ${token}`);
-});
+authRouter.get("/github-callback", authController.githubCallback);
 
 export default authRouter;
