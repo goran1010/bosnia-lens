642ac0beda7661809fdf173c1a002d8c9319b78d
Goran Jovic <goran1010jovic@gmail.com>
2025-12-08 19:45:57 +0100

    test: remove any new tests

--- frontend/tests/integration_tests/navigation.test.jsx
+++ frontend/tests/integration_tests/navigation.test.jsx
@@ -1,6 +1,5 @@
 import { test, describe, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import routes from "../../src/routes";
 import UserDataContext from "../../src/utils/UserDataContext";
@@ -105,74 +104,4 @@ describe("Navigation when loading a route", () => {
       expect(holidaysLink).toBeInTheDocument();
     }
   );
-});
-
-describe("Navigation when clicking links in Navbar", () => {
-  test.only("clicking Universities link navigates to Universities page", async () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ["/"],
-    });
-    render(<RouterProvider router={router} />);
-
-    const user = userEvent.setup();
-    const universitiesLink = await screen.findByRole("link", {
-      name: /Universities/i,
-    });
-    expect(universitiesLink).toBeInTheDocument();
-
-    await user.click(universitiesLink);
-
-    const text = await screen.findByText(/Universities Page/i);
-    expect(text).toBeInTheDocument();
-  });
-
-  test("clicking Postal Codes link navigates to Postal Codes page", async () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ["/"],
-    });
-    render(
-      <UserDataContext value={{ message: [], setMessage: () => {} }}>
-        <RouterProvider router={router} />
-      </UserDataContext>
-    );
-
-    const user = userEvent.setup();
-    const postalCodesLink = screen.getByRole("link", {
-      name: /Postal Codes/i,
-    });
-
-    await user.click(postalCodesLink);
-
-    screen.debug();
-    const linkElement = await screen.findByText(/Postal Code or Municipality/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-
-  test("clicking Holidays link navigates to Holidays page", async () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ["/"],
-    });
-    render(<RouterProvider router={router} />);
-
-    const user = userEvent.setup();
-    const holidaysLink = screen.getByRole("link", { name: /Holidays/i });
-    await user.click(holidaysLink);
-
-    const linkElements = await screen.findAllByText(/Holidays/i);
-    expect(linkElements[0]).toBeInTheDocument();
-  });
-
-  test("clicking Home link navigates to Home page", async () => {
-    const router = createMemoryRouter(routes, {
-      initialEntries: ["/postal-codes"],
-    });
-    render(<RouterProvider router={router} />);
-
-    const user = userEvent.setup();
-    const homeLink = screen.getByRole("link", { name: /Home/i });
-    await user.click(homeLink);
-
-    const linkElement = await screen.findByText(/Bosnia Lens/i);
-    expect(linkElement).toBeInTheDocument();
-  });
 });
