668fa0bfd124581cbfbfa0b8127a928a6c01c3a4
Goran Jovic <goran1010jovic@gmail.com>
2025-12-10 10:10:15 +0100

    test: add test to check validationMessage for username field
    Also refactored previous test to remove some duplicated code.

--- frontend/tests/unit_tests/SignUp.test.jsx
+++ frontend/tests/unit_tests/SignUp.test.jsx
@@ -15,8 +15,18 @@ beforeEach(() => {
   );
 });
 
+function createFormElements() {
+  return {
+    usernameField: screen.getByLabelText(/Username/i),
+    emailField: screen.getByLabelText(/Email/i),
+    passwordField: screen.getByLabelText("Password"),
+    confirmPasswordField: screen.getByLabelText(/Confirm Password/i),
+    signUpButton: screen.getByRole("button", { name: /Create/i }),
+  };
+}
+
 describe("Render SignUp Component", () => {
-  test("SignUp component", () => {
+  test("SignUp component heading", () => {
     const linkElement = screen.getByRole("heading", {
       name: /Create your account/i,
     });
@@ -24,38 +34,41 @@ describe("Render SignUp Component", () => {
   });
 
   test("renders SignUp form fields", () => {
-    const usernameField = screen.getByLabelText(/Username/i);
-    const emailField = screen.getByLabelText(/Email/i);
-    const passwordField = screen.getByLabelText("Password");
-    const confirmPasswordField = screen.getByLabelText(/Confirm Password/i);
-    const signUpButton = screen.getByRole("button", { name: /Create/i });
+    const formElements = createFormElements();
 
-    expect(usernameField).toBeInTheDocument();
-    expect(emailField).toBeInTheDocument();
-    expect(passwordField).toBeInTheDocument();
-    expect(confirmPasswordField).toBeInTheDocument();
-    expect(signUpButton).toBeInTheDocument();
+    for (let element in formElements) {
+      expect(formElements[element]).toBeInTheDocument();
+    }
   });
 });
 
 describe("User interaction with SignUp Component", () => {
   test("allows user to fill out and submit the SignUp form", async () => {
+    const formElements = createFormElements();
     const user = userEvent.setup();
+
+    await user.type(formElements.usernameField, "testuser");
+    await user.type(formElements.emailField, "testuser@example.com");
+    await user.type(formElements.passwordField, "Password123!");
+    await user.type(formElements.confirmPasswordField, "Password123!");
+
+    expect(formElements.usernameField).toHaveValue("testuser");
+    expect(formElements.emailField).toHaveValue("testuser@example.com");
+    expect(formElements.passwordField).toHaveValue("Password123!");
+    expect(formElements.confirmPasswordField).toHaveValue("Password123!");
+  });
+
+  test("shows validation messages for invalid input", async () => {
+    const user = userEvent.setup();
+
+    const button = screen.getByRole("button", { name: /Create/i });
     const usernameField = screen.getByLabelText(/Username/i);
-    const emailField = screen.getByLabelText(/Email/i);
-    const passwordField = screen.getByLabelText("Password");
-    const confirmPasswordField = screen.getByLabelText(/Confirm Password/i);
-    const signUpButton = screen.getByRole("button", { name: /Create/i });
 
-    await user.type(usernameField, "testuser");
-    await user.type(emailField, "testuser@example.com");
-    await user.type(passwordField, "password123");
-    await user.type(confirmPasswordField, "password123");
-    await user.click(signUpButton);
+    await user.type(usernameField, "test");
+    await user.click(button);
 
-    expect(usernameField).toHaveValue("testuser");
-    expect(emailField).toHaveValue("testuser@example.com");
-    expect(passwordField).toHaveValue("password123");
-    expect(confirmPasswordField).toHaveValue("password123");
+    expect(usernameField).toHaveValue("test");
+    // JSDOM doesn't render browser validation UI so check the input's validationMessage
+    expect(usernameField.validationMessage).toMatch(/at least 6 characters/i);
   });
 });
