96b970387af756348f2365a9fc1373f5de30b060
Goran Jovic <goran1010jovic@gmail.com>
2025-12-11 11:37:29 +0100

    test: add initial LogIn tests

--- frontend/tests/unit_tests/LogIn.test.jsx
+++ frontend/tests/unit_tests/LogIn.test.jsx
@@ -52,3 +52,51 @@ describe("User typing in input fields in LogIn Component", () => {
     expect(formElements.passwordField).toHaveValue("Password123!");
   });
 });
+
+describe("LogIn form validation", () => {
+  test("shows validation messages for invalid input", async () => {
+    const user = userEvent.setup();
+    const usernameField = screen.getByLabelText(/Username/i);
+    await user.type(usernameField, "test");
+    expect(usernameField).toHaveValue("test");
+    // JSDOM doesn't render browser validation UI so check the input's validationMessage
+    expect(usernameField.validationMessage).toMatch(/at least 6 characters/i);
+    await user.type(usernameField, "user");
+    expect(usernameField).toHaveValue("testuser");
+    expect(usernameField.validationMessage).toBe("");
+
+    const passwordField = screen.getByLabelText("Password");
+    await user.type(passwordField, "pass");
+    expect(passwordField).toHaveValue("pass");
+    expect(passwordField.validationMessage).toMatch(/at least 6 characters/i);
+    await user.type(passwordField, "word");
+    expect(passwordField).toHaveValue("password");
+    expect(passwordField.validationMessage).toBe("");
+  });
+
+  test("shows validation messages for invalid input on Login button click", async () => {
+    const logInButton = screen.getByRole("button", { name: /Log in/i });
+
+    const user = userEvent.setup();
+    const usernameField = screen.getByLabelText(/Username/i);
+    await user.type(usernameField, "test");
+    await user.click(logInButton);
+    expect(usernameField).toHaveValue("test");
+    // JSDOM doesn't render browser validation UI so check the input's validationMessage
+    expect(usernameField.validationMessage).toMatch(/at least 6 characters/i);
+    await user.type(usernameField, "user");
+    await user.click(logInButton);
+    expect(usernameField).toHaveValue("testuser");
+    expect(usernameField.validationMessage).toBe("");
+
+    const passwordField = screen.getByLabelText("Password");
+    await user.type(passwordField, "pass");
+    await user.click(logInButton);
+    expect(passwordField).toHaveValue("pass");
+    expect(passwordField.validationMessage).toMatch(/at least 6 characters/i);
+    await user.type(passwordField, "word");
+    await user.click(logInButton);
+    expect(passwordField).toHaveValue("password");
+    expect(passwordField.validationMessage).toBe("");
+  });
+});
