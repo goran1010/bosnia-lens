b28a7f80bdd22cd828af8b79fde8426bca47f99d
Goran Jovic <goran1010jovic@gmail.com>
2025-12-24 12:39:05 +0100

    chore: initial files form prisma update

--- backend/db/prisma.js
+++ backend/db/prisma.js
@@ -1,4 +1,5 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "../prisma/generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const DATABASE_URL = process.env.DATABASE_URL;
 const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
@@ -14,12 +15,7 @@ if (process.env.NODE_ENV === "test" && !TEST_DATABASE_URL) {
 const databaseUrl =
   process.env.NODE_ENV === "test" ? TEST_DATABASE_URL : DATABASE_URL;
 
-const prisma = new PrismaClient({
-  datasources: {
-    db: {
-      url: databaseUrl,
-    },
-  },
-});
+const adapter = new PrismaPg({ databaseUrl });
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
