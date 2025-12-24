60ec144293d20236d7ecdc4e94c82bdd5362ebd2
Goran Jovic <goran1010jovic@gmail.com>
2025-12-24 14:31:32 +0100

    chore: create all new files from prisma v7

--- backend/db/prisma.js
+++ backend/db/prisma.js
@@ -1,21 +1,9 @@
-import { PrismaClient } from "../prisma/generated/prisma/client.js";
 import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "../generated/prisma/client.js";
 
-const DATABASE_URL = process.env.DATABASE_URL;
-const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL;
+const connectionString = `${process.env.DATABASE_URL}`;
 
-if (!DATABASE_URL) {
-  throw new Error("Missing required environment variable: DATABASE_URL");
-}
-
-if (process.env.NODE_ENV === "test" && !TEST_DATABASE_URL) {
-  throw new Error("Missing required environment variable: TEST_DATABASE_URL");
-}
-
-const databaseUrl =
-  process.env.NODE_ENV === "test" ? TEST_DATABASE_URL : DATABASE_URL;
-
-const adapter = new PrismaPg({ databaseUrl });
+const adapter = new PrismaPg({ connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export default prisma;
--- backend/prisma/migrations/20251123123549_add_email_confirmed_to_user/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "User" ADD COLUMN     "isEmailConfirmed" BOOLEAN NOT NULL DEFAULT false;
