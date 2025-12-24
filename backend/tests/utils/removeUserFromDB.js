42661354f026b98c7de3f18c5314b85dace1dea5
Goran Jovic <goran1010jovic@gmail.com>
2025-12-24 11:40:17 +0100

    test: change delete user to deleteMany in removeUserFromDB

--- backend/tests/utils/removeUserFromDB.js
+++ backend/tests/utils/removeUserFromDB.js
@@ -1,5 +1,7 @@
 import prisma from "../../db/prisma";
 
 export default async function removeUserFromDB(newUser) {
-  return await prisma.user.delete({ where: { username: newUser.username } });
+  return await prisma.user.deleteMany({
+    where: { username: newUser.username },
+  });
 }
