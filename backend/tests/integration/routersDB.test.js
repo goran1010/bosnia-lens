fcbbb4631573f2e04a2b498036dca80a9e85c962
Goran Jovic <goran1010jovic@gmail.com>
2026-01-14 13:53:34 +0100

    test: change responseData to response

--- backend/tests/integration/routersDB.test.js
+++ backend/tests/integration/routersDB.test.js
@@ -48,16 +48,16 @@ describe("usersRouter", () => {
   test("responds with 200 and User test_user logged in successfully for correct input", async () => {
     const newUser = createNewUser();
 
-    const responseData = await createAndLoginUser(newUser);
+    const response = await createAndLoginUser(newUser);
 
     const expectedData = {
       message: `User ${newUser.username} logged in successfully`,
       accessToken: "randomstring",
     };
 
-    expect(responseData.status).toBe(200);
-    expect(responseData.body.message).toEqual(expectedData.message);
+    expect(response.status).toBe(200);
+    expect(response.body.message).toEqual(expectedData.message);
 
-    expect(responseData.body.data).toHaveProperty("accessToken");
+    expect(response.body.data).toHaveProperty("accessToken");
   });
 });
