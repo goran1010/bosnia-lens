96589628c39a1171e940165453e493257367a88e
Goran Jovic <goran1010jovic@gmail.com>
2026-01-31 23:12:34 +0100

    test: exclude generated prisma folder from test coverage

--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -3,5 +3,13 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globalSetup: "./tests/setup/globalSetup.js",
+    coverage: {
+      exclude: [
+        "tests/**",
+        "vitest.config.js",
+        "node_modules/**",
+        "generated/**",
+      ],
+    },
   },
 });
