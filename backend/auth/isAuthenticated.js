62f9215915692f5788a8cb25db49c7e8247b1256
Goran Jovic <goran1010jovic@gmail.com>
2026-02-10 09:14:35 +0100

    feat: modify JSON message from isAuthenticated and isNotAuthenticated

--- backend/auth/isAuthenticated.js
+++ backend/auth/isAuthenticated.js
@@ -1,7 +1,7 @@
 export default function isAuthenticated(req, res, next) {
   try {
     if (req.user) return next();
-    res.status(403).json({ message: "Access denied." });
+    res.status(403).json({ message: "You need to be logged in." });
   } catch (err) {
     next(err);
   }
