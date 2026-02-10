bf63227238bf3207bf9f0bb5ff8734cb0baf4cf0
Goran Jovic <goran1010jovic@gmail.com>
2026-02-10 14:23:04 +0100

    feat: modify response message in isAuthenticated middleware

--- backend/auth/isAuthenticated.js
+++ backend/auth/isAuthenticated.js
@@ -1,7 +1,7 @@
 export default function isAuthenticated(req, res, next) {
   try {
     if (req.user) return next();
-    res.status(403).json({ message: "You need to be logged in." });
+    res.status(403).json({ error: "You need to be logged in." });
   } catch (err) {
     next(err);
   }
