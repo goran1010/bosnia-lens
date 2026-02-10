9636620e9bb8b324114e1c0c6da344cae31f957a
Goran Jovic <goran1010jovic@gmail.com>
2026-02-10 09:12:32 +0100

    feat: change isAuthenticated to session-based authentication

--- backend/auth/isAuthenticated.js
+++ backend/auth/isAuthenticated.js
@@ -1,24 +1,8 @@
-import jwt from "jsonwebtoken";
-const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
-
 export default function isAuthenticated(req, res, next) {
-  if (!ACCESS_TOKEN_SECRET) {
-    return res.status(500).json({ error: "Server configuration error" });
+  try {
+    if (req.user) return next();
+    res.status(403).json({ message: "Access denied." });
+  } catch (err) {
+    next(err);
   }
-
-  const authHeader = req.headers["authorization"];
-  const token = authHeader && authHeader.split(" ")[1];
-  if (!token) {
-    return res.status(401).json({ error: "Need to be logged in" });
-  }
-
-  jwt.verify(token, ACCESS_TOKEN_SECRET, (err, token) => {
-    if (err) {
-      return res
-        .status(403)
-        .json({ error: "Incorrect or expired session token" });
-    }
-    req.token = token;
-    next();
-  });
 }
