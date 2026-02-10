41d0d2892bfc13e0e885c7566dcb3ab03a1799c2
Goran Jovic <goran1010jovic@gmail.com>
2026-02-10 09:25:33 +0100

    fix: change name of isNotAuthenticated function

--- backend/auth/isNotAuthenticated.js
+++ backend/auth/isNotAuthenticated.js
@@ -1,4 +1,4 @@
-export default function isNotLoggedIn(req, res, next) {
+export default function isNotAuthenticated(req, res, next) {
   if (!req.user) return next();
   res.status(403).json({ message: "You are already logged in." });
 }
