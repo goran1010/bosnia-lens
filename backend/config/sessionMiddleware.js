df28442ffa5656cdd09d5103c3da6e29b00cfa81
Goran Jovic <goran1010jovic@gmail.com>
2026-02-16 09:44:27 +0100

    fix: update session settings

--- backend/config/sessionMiddleware.js
+++ backend/config/sessionMiddleware.js
@@ -1,5 +1,6 @@
 const NUMBER_OF_DAYS = 30;
 const COOKIE_SECRET = process.env.COOKIE_SECRET;
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 import expressSession from "express-session";
 import { PrismaSessionStore } from "@quixo3/prisma-session-store";
@@ -8,8 +9,8 @@ import prisma from "../db/prisma.js";
 const sessionMiddleware = expressSession({
   cookie: {
     maxAge: NUMBER_OF_DAYS * 24 * 60 * 60 * 1000,
-    sameSite: "none",
-    secure: true,
+    sameSite: IS_PRODUCTION ? "none" : "lax",
+    secure: IS_PRODUCTION,
     httpOnly: true,
   },
   secret: COOKIE_SECRET,
