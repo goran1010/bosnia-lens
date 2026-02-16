8e85b24af32c2795fffe993b3a7d869178b4ff36
Goran Jovic <goran1010jovic@gmail.com>
2026-02-16 15:34:15 +0100

    fix: remove options from clearCookie temporarily

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -6,6 +6,7 @@ import emailConfirmHTML from "../utils/emailConfirmHTML.js";
 import passport from "../config/passport.js";
 
 const NUMBER_OF_DAYS = 30;
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
 
 const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET;
 
@@ -117,13 +118,7 @@ export function logout(req, res) {
     }
 
     req.session.destroy(() => {
-      res.clearCookie("connect.sid", {
-        maxAge: NUMBER_OF_DAYS * 24 * 60 * 60 * 1000,
-        sameSite: "none",
-        secure: true,
-        httpOnly: true,
-        path: "/",
-      });
+      res.clearCookie("connect.sid");
       res.json({ message: "User logged out successfully" });
     });
   });
