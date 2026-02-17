18ae6150756c4b06a620be238bd64776d60ff3a4
Goran Jovic <goran1010jovic@gmail.com>
2026-02-17 15:50:50 +0100

    feat: add select dataset form

--- frontend/src/components/AdminDashboard/AdminForm.jsx
+++ frontend/src/components/AdminDashboard/AdminForm.jsx
@@ -1,5 +1,31 @@
+import { useState } from "react";
+
 function AdminForm() {
-  return <form></form>;
+  const [datasetSelect, setDatasetSelect] = useState("");
+
+  function handleDataset(e) {
+    setDatasetSelect(e.target.value);
+  }
+
+  return (
+    <form>
+      <div>
+        <label htmlFor="dataset">Choose dataset: </label>
+        <select name="dataset" id="dataset" onChange={handleDataset}>
+          <option default value="">
+            Select a dataset
+          </option>
+          <option value="postal-codes">Postal Codes</option>
+          <option value="holidays">Holidays</option>
+        </select>
+      </div>
+      {datasetSelect ? (
+        <div>Hey</div>
+      ) : (
+        <div>You need to select a dataset.</div>
+      )}
+    </form>
+  );
 }
 
 export { AdminForm };
