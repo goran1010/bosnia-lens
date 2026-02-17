c287f5ce55ad1bbd3771ae31409d08c1bf99aff7
Goran Jovic <goran1010jovic@gmail.com>
2026-02-17 10:32:24 +0100

    feat: change HTML element being returned from AdminDashboard

--- frontend/src/components/AdminDashboard/AdminDashboard.jsx
+++ frontend/src/components/AdminDashboard/AdminDashboard.jsx
@@ -4,9 +4,9 @@ import UserDataContext from "../../utils/UserDataContext";
 function AdminDashboard() {
   const { userData } = useContext(UserDataContext);
 
-  if (userData?.isAdmin) return <div>Admin Dashboard</div>;
+  if (userData?.isAdmin) return <>Admin Dashboard</>;
 
-  return <div>User needs to an admin to see the dashboard.</div>;
+  return <>User needs to an admin to see the dashboard.</>;
 }
 
 export { AdminDashboard };
