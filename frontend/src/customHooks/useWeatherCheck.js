ccea0a9fefab1c2ea25515466e34aacf2d1413cc
Goran Jovic <goran1010jovic@gmail.com>
2026-02-20 16:49:10 +0100

    feat: add notifications to useWeatherCheck

--- frontend/src/customHooks/useWeatherCheck.js
+++ frontend/src/customHooks/useWeatherCheck.js
@@ -1,6 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
+import NotificationContext from "../utils/NotificationContext";
 
 export default function useWeatherCheck(setWeatherForecast, setLoading) {
+  const { addNotification } = useContext(NotificationContext);
   const URL = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/bosnia?unitGroup=metric&include=current&key=RK9QGT68LJC4A8CZLA785FREP&contentType=json`;
 
   useEffect(() => {
@@ -8,6 +10,10 @@ export default function useWeatherCheck(setWeatherForecast, setLoading) {
       try {
         const response = await fetch(URL, { mode: "cors" });
         if (!response.ok) {
+          addNotification({
+            type: "error",
+            message: "Failed to fetch weather data. Please try again later.",
+          });
           return console.warn(response);
         }
 
@@ -16,9 +22,16 @@ export default function useWeatherCheck(setWeatherForecast, setLoading) {
           const URL = `https://raw.githubusercontent.com/visualcrossing/WeatherIcons/58c79610addf3d4d91471abbb95b05e96fb43019/SVG/1st%20Set%20-%20Color/${day.icon}.svg`;
           day.iconURL = URL;
         }
-
+        addNotification({
+          type: "success",
+          message: "Weather data fetched successfully!",
+        });
         setWeatherForecast(data.days);
       } catch (err) {
+        addNotification({
+          type: "error",
+          message: "An error occurred while fetching weather data.",
+        });
         console.error(err);
       } finally {
         setLoading(false);
