2daf257d1b20a517ec2bb4e2210c6af42ad5f71d
Goran Jovic <goran1010jovic@gmail.com>
2026-02-20 12:03:06 +0100

    feat: change Root to remove old Messages and add Notifications

--- frontend/src/Root.jsx
+++ frontend/src/Root.jsx
@@ -1,28 +1,58 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer";
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import UserDataContext from "./utils/UserDataContext";
 import Spinner from "@goran1010/spinner";
 import useStatusCheck from "./customHooks/useStatusCheck";
+import Notifications from "./components/Notifications";
+import NotificationContext from "./utils/NotificationContext";
 
 function Root() {
   const [userData, setUserData] = useState(null);
-  const [message, setMessage] = useState([]);
+  const [notifications, setNotifications] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useStatusCheck(setUserData, setLoading);
 
+  const addNotification = ({ type = "info", message, duration = 5000 }) => {
+    const newNotification = {
+      id: crypto.randomUUID(),
+      type,
+      message,
+      duration,
+      createdAt: Date.now(),
+    };
+
+    setNotifications((prev) => [...prev, newNotification]);
+  };
+
+  const removeNotification = (id) => {
+    setNotifications((prev) => prev.filter((n) => n.id !== id));
+  };
+
+  const notificationValue = useMemo(
+    () => ({
+      notifications,
+      addNotification,
+      removeNotification,
+    }),
+    [notifications],
+  );
+
   return (
-    <UserDataContext value={{ userData, setUserData, message, setMessage }}>
-      <>
-        <Navbar />
-        <main className="relative flex flex-col flex-auto max-w-230 m-auto gap-5">
-          {loading ? <Spinner /> : <Outlet />}
-        </main>
-        <Footer />
-      </>
-    </UserDataContext>
+    <NotificationContext value={{ notificationValue }}>
+      <UserDataContext value={{ userData, setUserData }}>
+        <>
+          <Navbar />
+          <Notifications />
+          <main className="relative flex flex-col flex-auto max-w-230 m-auto gap-5">
+            {loading ? <Spinner /> : <Outlet />}
+          </main>
+          <Footer />
+        </>
+      </UserDataContext>
+    </NotificationContext>
   );
 }
 
