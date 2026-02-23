95661d2778ce333b35c2ca1969f5245d2abf6dd1
Goran Jovic <goran1010jovic@gmail.com>
2026-02-23 14:45:40 +0100

    test: add more render tests for AdminDashboard component

--- frontend/__tests__/unit/AdminDashboard.test.jsx
+++ frontend/__tests__/unit/AdminDashboard.test.jsx
@@ -3,8 +3,8 @@ import { render, screen } from "@testing-library/react";
 import { AdminDashboard } from "../../src/components/AdminDashboard/AdminDashboard";
 import { UserDataContext } from "../../src/utils/UserDataContext";
 
-describe("AdminDashboard component", () => {
-  test("render AdminDashboard component when user not logged in", () => {
+describe("render AdminDashboard component", () => {
+  test("when user not logged in", () => {
     render(
       <UserDataContext value={{ userData: null }}>
         <AdminDashboard />
@@ -15,5 +15,34 @@ describe("AdminDashboard component", () => {
       /User needs to an admin to see the dashboard./i,
     );
     expect(linkElement).toBeInTheDocument();
+  });
+
+  test("when user logged in but not admin", () => {
+    render(
+      <UserDataContext value={{ userData: { isAdmin: false } }}>
+        <AdminDashboard />
+      </UserDataContext>,
+    );
+
+    const linkElement = screen.getByText(
+      /User needs to an admin to see the dashboard./i,
+    );
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("when user logged in and is admin", () => {
+    render(
+      <UserDataContext value={{ userData: { isAdmin: true } }}>
+        <AdminDashboard />
+      </UserDataContext>,
+    );
+
+    const dataSetLabel = screen.getByLabelText(/Choose dataset:/i);
+    expect(dataSetLabel).toBeInTheDocument();
+
+    const linkElement = screen.queryByText(
+      /User needs to an admin to see the dashboard./i,
+    );
+    expect(linkElement).not.toBeInTheDocument();
   });
 });
