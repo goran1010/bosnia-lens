ebfc9778cf7fa7fbc600758330ff4c50d63b4424
Goran Jovic <goran1010jovic@gmail.com>
2026-02-24 09:33:05 +0100

    test: add tests for AdminDashboard component options selection

--- frontend/__tests__/unit/AdminDashboard.test.jsx
+++ frontend/__tests__/unit/AdminDashboard.test.jsx
@@ -1,7 +1,11 @@
 import { describe, test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { AdminDashboard } from "../../src/components/AdminDashboard/AdminDashboard";
 import { UserDataContext } from "../../src/utils/UserDataContext";
+import { NotificationContext } from "../../src/utils/NotificationContext";
+
+const user = userEvent.setup();
 
 describe("render AdminDashboard component", () => {
   test("when user not logged in", () => {
@@ -43,6 +47,38 @@ describe("render AdminDashboard component", () => {
     const linkElement = screen.queryByText(
       /User needs to an admin to see the dashboard./i,
     );
+    expect(linkElement).not.toBeInTheDocument();
+  });
+});
+
+describe("render AdminDashboard page when no dataset is selected", () => {
+  test("when no dataset is selected", () => {
+    render(
+      <UserDataContext value={{ userData: { isAdmin: true } }}>
+        <AdminDashboard />
+      </UserDataContext>,
+    );
+
+    const linkElement = screen.getByText(/You need to select a dataset./i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  test("when postal-codes dataset is selected", async () => {
+    render(
+      <NotificationContext value={{ addNotification: () => {} }}>
+        <UserDataContext value={{ userData: { isAdmin: true } }}>
+          <AdminDashboard />
+        </UserDataContext>
+        ,
+      </NotificationContext>,
+    );
+
+    const selectElement = screen.getByRole("combobox");
+    expect(selectElement).toBeInTheDocument();
+
+    await user.selectOptions(selectElement, "postal-codes");
+
+    const linkElement = screen.queryByText(/You need to select a dataset./i);
     expect(linkElement).not.toBeInTheDocument();
   });
 });
