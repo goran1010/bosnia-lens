926fdd91845c7df5bd81c02f09bc7fd07284982e
Goran Jovic <goran1010jovic@gmail.com>
2026-03-05 10:12:11 +0100

    feat: add global setup and teardown to clear db

--- backend/__tests__/setup/globalSetup.js
+++ backend/__tests__/setup/globalSetup.js
@@ -1,3 +1,12 @@
-async function setup() {}
+import * as usersModel from "../../models/usersModel.js";
 
-export { setup };
+export async function setup() {
+  process.env.NODE_ENV = "test";
+  await usersModel.deleteAll();
+  console.log("Database cleared before tests");
+}
+
+export async function teardown() {
+  console.log("Global teardown: Database cleared after tests");
+  await usersModel.deleteAll();
+}
