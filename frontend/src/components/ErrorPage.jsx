9688c13fcf70e388f299d4f77774aa8262697a68
Goran Jovic <goran1010jovic@gmail.com>
2026-03-07 12:07:16 +0100

    feat: add page title bar

--- frontend/src/components/ErrorPage.jsx
+++ frontend/src/components/ErrorPage.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 function ErrorPage() {
   return (
-    <div className="error-page">
-      <p>There is nothing here, sorry.</p>
-      <p>
-        <Link to="/">Go Home</Link>
-      </p>
-    </div>
+    <main className="flex flex-col items-center gap-4 justify-center h-screen">
+      <p className="text-gray-700 text-2xl">There is nothing here, sorry.</p>
+      <Link to="/" className="text-blue-500 underline text-2xl">
+        Go Home
+      </Link>
+    </main>
   );
 }
 export { ErrorPage };
