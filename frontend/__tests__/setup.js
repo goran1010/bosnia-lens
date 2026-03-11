36a2b59ffcdbc762a5f3041d28f93cc72b1f99a8
Goran Jovic <goran1010jovic@gmail.com>
2026-03-11 12:05:30 +0100

    test: fix login tests

--- frontend/__tests__/setup.js
+++ frontend/__tests__/setup.js
@@ -4,6 +4,18 @@ import * as matchers from "@testing-library/jest-dom/matchers";
 
 expect.extend(matchers);
 
+Object.defineProperty(window, "matchMedia", {
+  writable: true,
+  value: (query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addEventListener: () => {},
+    removeEventListener: () => {},
+    dispatchEvent: () => {},
+  }),
+});
+
 afterEach(() => {
   cleanup();
 });
