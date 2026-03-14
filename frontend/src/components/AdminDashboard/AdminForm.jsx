8a8634954e571f0761b829318514484eae16e127
Goran Jovic <goran1010jovic@gmail.com>
2026-03-14 12:10:05 +0100

    feat: modify colors in admin dashboard

--- frontend/src/components/AdminDashboard/AdminForm.jsx
+++ frontend/src/components/AdminDashboard/AdminForm.jsx
@@ -7,8 +7,8 @@ function AdminForm() {
     useGetAllContributors();
 
   return (
-    <div className="relative min-h-full w-full flex items-center justify-center bg-gray-50 rounded-md p-3 dark:bg-gray-800">
-      <section className="w-full max-w-6xl p-4 md:p-6 flex flex-col gap-4 bg-white border border-gray-300 rounded-lg shadow dark:bg-gray-700 dark:border-gray-600 dark:text-white">
+    <div className="relative min-h-full w-full flex items-center justify-center bg-gray-300 rounded-md p-3 dark:bg-gray-800">
+      <section className="w-full max-w-6xl p-4 md:p-6 flex flex-col gap-4 bg-gray-400 border border-gray-300 rounded-lg shadow dark:bg-gray-700 dark:border-gray-600 dark:text-white">
         <h1 className="text-center text-2xl font-bold text-gray-800 dark:text-white pb-2 border-b-2 border-gray-300 dark:border-gray-600">
           Admin Dashboard
         </h1>
