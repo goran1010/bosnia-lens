f601fbc1b75a7032da41f32cffd2b5abb8256a70
Goran Jovic <goran1010jovic@gmail.com>
2026-03-17 15:02:13 +0100

    test: move Navbar test to its own file

--- frontend/__tests__/unit/components/Navbar.test.jsx
+++ frontend/__tests__/unit/components/Navbar.test.jsx
@@ -1,26 +1,16 @@
 import { test, describe, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
-import { Home } from "../../src/components/Home/Home";
-import { Holidays } from "../../src/components/Holidays";
-import { PostalCodes } from "../../src/components/PostalCodes/PostalCodes";
-import { Universities } from "../../src/components/Universities";
-import { Footer } from "../../src/components/Footer";
-import { Navbar } from "../../src/components/Navbar/Navbar";
+import { Navbar } from "../../../src/components/Navbar/Navbar";
 import { MemoryRouter } from "react-router-dom";
-import { UserDataContext } from "../../src/contextData/UserDataContext";
-import { NotificationContext } from "../../src/contextData/NotificationContext";
+import { UserDataContext } from "../../../src/contextData/UserDataContext";
 
 describe("Render Components", () => {
-  const contextValue = {};
-
   test("Navbar component", () => {
     const userData = { username: "testuser", setUserData: () => {} };
     render(
       <MemoryRouter>
         <UserDataContext value={{ userData }}>
-          <NotificationContext value={contextValue}>
-            <Navbar />
-          </NotificationContext>
+          <Navbar />
         </UserDataContext>
       </MemoryRouter>,
     );
