0a449a6270d19c1d520e06f4bc28a47e2ebe0ccb
Goran Jovic <goran1010jovic@gmail.com>
2026-03-21 13:15:59 +0100

    fix: comment out server close code to prevent reload issues

--- backend/index.js
+++ backend/index.js
@@ -2,17 +2,17 @@ import { app } from "./app.js";
 
 const PORT = process.env.PORT;
 
-const server = app.listen(PORT, (error) => {
+app.listen(PORT, (error) => {
   if (error) throw error;
   // eslint-disable-next-line no-console
   console.log(`App started at port: ${PORT}`);
 });
 
-process.on("SIGTERM", () => {
-  console.warn("SIGTERM received. Shutting down gracefully...");
+// process.on("SIGTERM", () => {
+//   console.warn("SIGTERM received. Shutting down gracefully...");
 
-  server.close(() => {
-    console.warn("Process terminated");
-    process.exit(0);
-  });
-});
+//   server.close(() => {
+//     console.warn("Process terminated");
+//     process.exit(0);
+//   });
+// });
