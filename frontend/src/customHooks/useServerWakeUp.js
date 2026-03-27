b593974e62dddba20ac2d0cad8e36cefb4a3ece6
Goran Jovic <goran1010jovic@gmail.com>
2026-03-27 09:41:26 +0100

    refactor: useServerWakeUp - regular attempts to contact the server

--- frontend/src/customHooks/useServerWakeUp.js
+++ frontend/src/customHooks/useServerWakeUp.js
@@ -1,8 +1,9 @@
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 const URL = import.meta.env.VITE_BACKEND_URL;
+const ALLOWED_ATTEMPTS = 30;
+const DELAY_BETWEEN_ATTEMPTS = 2000;
 
 function useServerWakeUp({ setLongWait, setServerIsDown }) {
-  const lastAttempt = useRef(false);
   useEffect(() => {
     // Limit the number of wake-up attempts to prevent infinite loops
     let numberOfAttempts = 0;
@@ -11,23 +12,12 @@ function useServerWakeUp({ setLongWait, setServerIsDown }) {
       setLongWait(true);
     }, 4000);
 
-    const reloadTimer = setTimeout(() => {
-      lastAttempt.current = true;
-      checkServer();
-    }, 20000);
-
     async function checkServer() {
-      if (numberOfAttempts >= 3) {
-        if (lastAttempt.current) {
-          setServerIsDown(true);
-          setLongWait(false);
-          console.error(
-            "Server is down after multiple attempts. Please try again later.",
-          );
-          return;
-        }
+      if (numberOfAttempts >= ALLOWED_ATTEMPTS) {
+        setServerIsDown(true);
+        setLongWait(false);
         console.error(
-          "Server is taking too long to wake up. Will attempt one more time.",
+          "Server can't be reached after multiple attempts. Please try again later.",
         );
         return;
       }
@@ -41,26 +31,24 @@ function useServerWakeUp({ setLongWait, setServerIsDown }) {
           setTimeout(() => {
             numberOfAttempts++;
             checkServer();
-          }, 2000);
+          }, DELAY_BETWEEN_ATTEMPTS);
           return;
         }
 
         setLongWait(false);
         clearTimeout(longWaitTimer);
-        clearTimeout(reloadTimer);
       } catch (err) {
         console.error("Error waking up server:", err);
         setTimeout(() => {
           numberOfAttempts++;
           checkServer();
-        }, 2000);
+        }, DELAY_BETWEEN_ATTEMPTS);
       }
     }
 
     checkServer();
     return () => {
       clearTimeout(longWaitTimer);
-      clearTimeout(reloadTimer);
     };
   }, [setLongWait, setServerIsDown]);
 }
