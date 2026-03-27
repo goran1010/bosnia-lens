6f6283bc5174a0361681b3fb56b581ad9fc74a9d
Goran Jovic <goran1010jovic@gmail.com>
2026-03-27 09:37:45 +0100

    refactor: fix grammar in long wait and server down messages

--- frontend/src/utils/longWaitInfo.jsx
+++ frontend/src/utils/longWaitInfo.jsx
@@ -4,7 +4,7 @@ function LongWaitInfo({ serverIsDown }) {
       <div className="fixed w-full h-full flex items-center justify-center p-2 md:p-4 lg:p-6 xl:p-8 2xl:p-10 bg-gray-100 dark:bg-gray-900">
         <div className="bg-red-500 p-2 rounded shadow">
           <p className="text-white font-bold text-center">
-            Server is currently down. Please try again later.
+            The server is currently down. Please try again later.
           </p>
         </div>
       </div>
@@ -15,8 +15,8 @@ function LongWaitInfo({ serverIsDown }) {
     <div className="fixed w-full h-full flex items-center justify-center p-2 md:p-4 lg:p-6 xl:p-8 2xl:p-10 bg-gray-100 dark:bg-gray-900">
       <div className="bg-gray-500 p-2 rounded shadow">
         <p className="text-white font-bold text-center">
-          Getting response from server is taking longer than expected (server
-          might be waking up). Please wait...
+          Getting response from the server is taking longer than expected
+          (server might be waking up). Please wait...
         </p>
       </div>
     </div>
