3f3283710f9b8070d6a956fe7217c69d5311b77a
Goran Jovic <goran1010jovic@gmail.com>
2026-03-31 12:07:48 +0200

    fix: remove trim from Button and Select components

--- frontend/src/components/sharedComponents/Button.jsx
+++ frontend/src/components/sharedComponents/Button.jsx
@@ -3,11 +3,7 @@ function Button({ children, className = "", type = "button", ...props }) {
     "w-full relative inline-flex items-center justify-center rounded-md bg-blue-600 p-2 text-white transition transform hover:cursor-pointer hover:bg-blue-700 disabled:cursor-not-allowed disabled:bg-blue-500 disabled:text-gray-300 not-disabled:active:scale-95";
 
   return (
-    <button
-      type={type}
-      className={`${baseClassName} ${className}`.trim()}
-      {...props}
-    >
+    <button type={type} className={`${baseClassName} ${className}`} {...props}>
       {children}
     </button>
   );
--- frontend/src/components/sharedComponents/Select.jsx
+++ frontend/src/components/sharedComponents/Select.jsx
@@ -3,7 +3,7 @@ function Select({ children, className = "", ...props }) {
     "text-gray-800 dark:text-gray-200 p-2 rounded cursor-pointer border border-gray-400 dark:border-gray-600 focus:outline-none focus:ring-2 focus:ring-blue-500";
 
   return (
-    <select className={`${baseClassName} ${className}`.trim()} {...props}>
+    <select className={`${baseClassName} ${className}`} {...props}>
       {children}
     </select>
   );
