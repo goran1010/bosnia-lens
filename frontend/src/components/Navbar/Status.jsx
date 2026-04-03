25adbfcd819d3f80427ed43cf8f3098f82599097
Goran Jovic <goran1010jovic@gmail.com>
2026-04-03 11:18:29 +0200

    refactor: modify the Status component

--- frontend/src/components/Navbar/Status.jsx
+++ frontend/src/components/Navbar/Status.jsx
@@ -9,7 +9,7 @@ function Status() {
     return (
       <div className="flex items-center justify-center">
         <Link
-          className="block py-3 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 cursor-pointer"
+          className="block py-3 px-4 hover:bg-gray-400 dark:hover:bg-gray-700 cursor-pointer rounded-lg lg:rounded-none lg:px-2"
           to="/profile"
         >
           Profile
@@ -21,7 +21,7 @@ function Status() {
   return (
     <div className="flex items-center justify-center">
       <Link
-        className="block py-3 px-2 hover:bg-gray-400 dark:hover:bg-gray-700"
+        className="block py-3 px-2 hover:bg-gray-400 dark:hover:bg-gray-700 cursor-pointer rounded-lg lg:rounded-none lg:px-2"
         to="/login"
       >
         Log In
