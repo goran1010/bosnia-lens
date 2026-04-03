7c9b56d25ed48287590fa1cd92734156f8b67d57
Goran Jovic <goran1010jovic@gmail.com>
2026-04-03 11:06:55 +0200

    fix: modify hover colors for navbar buttons

--- frontend/src/components/Navbar/ButtonNavbar.jsx
+++ frontend/src/components/Navbar/ButtonNavbar.jsx
@@ -2,7 +2,7 @@ function ButtonNavbar({ children, className, ...rest }) {
   return (
     <button
       className={`bg-gray-300 dark:bg-gray-800 border border-gray-500 dark:border-gray-400 text-gray-800 dark:text-white min-w-20
-        w-full relative inline-flex items-center justify-center rounded-md p-2 transition transform hover:cursor-pointer hover:bg-gray-700
+        w-full relative inline-flex items-center justify-center rounded-md p-2 transition transform hover:cursor-pointer hover:bg-gray-500
         disabled:cursor-not-allowed disabled:bg-blue-500 disabled:text-gray-300 not-disabled:active:scale-95
          ${className}`}
       {...rest}
--- frontend/src/components/Navbar/MobileMenu.jsx
+++ frontend/src/components/Navbar/MobileMenu.jsx
@@ -7,63 +7,55 @@ function MobileMenu({ setIsMenuOpen, userData }) {
       className="z-10 pb-2 absolute top-full bg-gray-200 w-full dark:bg-gray-800 dark:text-white left-0"
     >
       <ul className="flex flex-col items-center">
-        <li>
-          <Link
-            className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700"
-            to="/"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Home
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700 text-nowrap"
-            to="/postal-codes"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Postal Codes
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700"
-            to="/holidays"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Holidays
-          </Link>
-        </li>
-        <li>
-          <Link
-            className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700"
-            to="/universities"
-            onClick={() => setIsMenuOpen(false)}
-          >
-            Universities
-          </Link>
-        </li>
+        <Link
+          className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700 w-full text-center text-nowrap"
+          to="/"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Home
+        </Link>
+
+        <Link
+          className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700 w-full text-center text-nowrap"
+          to="/postal-codes"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Postal Codes
+        </Link>
+
+        <Link
+          className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700 w-full text-center text-nowrap"
+          to="/holidays"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Holidays
+        </Link>
+
+        <Link
+          className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700 w-full text-center text-nowrap"
+          to="/universities"
+          onClick={() => setIsMenuOpen(false)}
+        >
+          Universities
+        </Link>
+
         {(userData?.role === "ADMIN" || userData?.role === "CONTRIBUTOR") && (
-          <li>
-            <Link
-              className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700"
-              to="/contributor-dashboard"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Contributor
-            </Link>
-          </li>
+          <Link
+            className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700 w-full text-center text-nowrap"
+            to="/contributor-dashboard"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Contributor
+          </Link>
         )}
         {userData?.role === "ADMIN" && (
-          <li>
-            <Link
-              className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700"
-              to="/admin-dashboard"
-              onClick={() => setIsMenuOpen(false)}
-            >
-              Admin
-            </Link>
-          </li>
+          <Link
+            className="block p-2 hover:bg-gray-400 dark:hover:bg-gray-700 w-full text-center text-nowrap"
+            to="/admin-dashboard"
+            onClick={() => setIsMenuOpen(false)}
+          >
+            Admin
+          </Link>
         )}
       </ul>
     </div>
