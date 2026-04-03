fcaad5cb316b1b02122acd7917cd052cfd0de9d9
Goran Jovic <goran1010jovic@gmail.com>
2026-04-03 10:59:32 +0200

    refactor: add new ButtonNavbar component and replace Button in Navbar

---
+++ frontend/src/components/Navbar/ButtonNavbar.jsx
@@ -0,0 +1,15 @@
+function ButtonNavbar({ children, className, ...rest }) {
+  return (
+    <button
+      className={`bg-gray-300 dark:bg-gray-800 border border-gray-500 dark:border-gray-400 text-gray-800 dark:text-white min-w-20
+        w-full relative inline-flex items-center justify-center rounded-md p-2 transition transform hover:cursor-pointer hover:bg-gray-700
+        disabled:cursor-not-allowed disabled:bg-blue-500 disabled:text-gray-300 not-disabled:active:scale-95
+         ${className}`}
+      {...rest}
+    >
+      {children}
+    </button>
+  );
+}
+
+export { ButtonNavbar };
--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ import { useTheme } from "../../utils/useTheme";
 import { ThemeSwitcher } from "./ThemeSwitcher";
 import { MobileMenu } from "./MobileMenu";
 import { StandardMenu } from "./StandardMenu";
-import { Button } from "../sharedComponents/Button";
+import { ButtonNavbar } from "./ButtonNavbar";
 
 function Navbar({
   isMenuOpen,
@@ -19,16 +19,18 @@ function Navbar({
   return (
     <nav className="relative px-2 bg-gray-300 w-full dark:bg-gray-800 dark:text-white font-bold grid grid-cols-3 lg:flex lg:justify-between items-center gap-2 transition transform">
       <div className="relative">
-        <Button
-          className="bg-gray-300 dark:bg-gray-800 border border-gray-500 dark:border-gray-400 text-gray-800 dark:text-white min-w-20"
+        <ButtonNavbar
           id="theme-switcher"
+          aria-label="Toggle theme menu"
+          aria-controls="theme-menu"
+          aria-expanded={isThemeMenuOpen}
           onClick={() => {
             setThemeMenuOpen((prev) => !prev);
             setIsMenuOpen(false);
           }}
         >
           {isThemeMenuOpen ? "Choose" : "Theme"}
-        </Button>
+        </ButtonNavbar>
         {isThemeMenuOpen && (
           <ThemeSwitcher
             setMode={setMode}
@@ -37,9 +39,8 @@ function Navbar({
         )}
       </div>
       <div className="flex lg:hidden justify-center items-center py-2">
-        <Button
+        <ButtonNavbar
           type="button"
-          className="py-2 px-3 rounded border border-gray-500 dark:border-gray-400 bg-gray-300 w-full dark:bg-gray-800 dark:text-white text-gray-800"
           aria-label="Toggle navigation menu"
           aria-controls="mobile-menu"
           aria-expanded={isMenuOpen}
@@ -49,7 +50,7 @@ function Navbar({
           }}
         >
           {isMenuOpen ? "Close" : "Menu"}
-        </Button>
+        </ButtonNavbar>
       </div>
 
       {isMenuOpen && (
