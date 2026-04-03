3057d53c0d94b9c1af0da4c0bf74e079f0e07d23
Goran Jovic <goran1010jovic@gmail.com>
2026-04-03 08:53:09 +0200

    feat: add notifications for contributors errors

--- backend/controllers/contributorController.js
+++ backend/controllers/contributorController.js
@@ -1,8 +1,9 @@
 import { postalCodesModel } from "../models/postalCodesModel.js";
+import { matchedData } from "express-validator";
 
 class ContributorController {
   async createPostalCode(req, res) {
-    const { city, code, post } = req.query;
+    const { city, code, post } = matchedData(req);
 
     const result = await postalCodesModel.createNew(city, code, post);
     res
