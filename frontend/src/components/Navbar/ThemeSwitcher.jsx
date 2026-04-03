6f83976e29077d9e26bcc877d3700b99108eed91
Goran Jovic <goran1010jovic@gmail.com>
2026-04-03 11:00:41 +0200

    fix: prevent user selection on theme switcher

--- frontend/src/components/Navbar/ThemeSwitcher.jsx
+++ frontend/src/components/Navbar/ThemeSwitcher.jsx
@@ -5,7 +5,7 @@ function ThemeSwitcher({ setMode, setThemeMenuOpen }) {
   const { addNotification } = useContext(NotificationContext);
 
   return (
-    <div className="z-10 pb-2 absolute top-full bg-gray-300 dark:bg-gray-800 dark:text-white left-0 w-full text-center rounded-b">
+    <div className="z-10 pb-2 absolute top-full bg-gray-300 dark:bg-gray-800 dark:text-white left-0 w-full text-center rounded-b user-select-none cursor-pointer">
       <ul className="flex flex-col">
         <li
           className="block py-2 px-4 hover:bg-gray-400 dark:hover:bg-gray-700"
