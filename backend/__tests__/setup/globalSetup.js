7922f3dce1bd56659ba0245c05fb2a8b95623126
Goran Jovic <goran1010jovic@gmail.com>
2026-04-06 08:42:29 +0200

    test: fix remaining backend tests

--- backend/__tests__/setup/globalSetup.js
+++ backend/__tests__/setup/globalSetup.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-console */
-
 import { usersModel } from "../../models/usersModel.js";
 
-export async function setup() {
+export default async function () {
   process.env.NODE_ENV = "test";
-  await usersModel.deleteAll();
-  console.log("Database cleared before tests");
-}
 
-export async function teardown() {
-  console.log("Global teardown: Database cleared after tests");
   await usersModel.deleteAll();
+  console.log("Test database cleared before tests");
+
+  return async () => {
+    console.log("Global teardown: Test database cleared after tests");
+    await usersModel.deleteAll();
+  };
 }
--- backend/vitest.config.js
+++ backend/vitest.config.js
@@ -3,6 +3,7 @@ import { defineConfig } from "vitest/config";
 export default defineConfig({
   test: {
     globalSetup: "./__tests__/setup/globalSetup.js",
+    setupFiles: "./__tests__/setup/setup.js",
     coverage: {
       exclude: [
         "__tests__/**",
