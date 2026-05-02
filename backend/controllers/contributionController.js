37acc893dac35986a03755c9cc98d0456128212f
Goran Jovic <goran1010jovic@gmail.com>
2026-05-02 12:38:28 +0200

    fix: parse code as number and add type of change to pending changes model

--- backend/controllers/contributionController.js
+++ backend/controllers/contributionController.js
@@ -1,46 +1,72 @@
 import { pendingChangesPostalCodeModel } from "../models/pendingChangesPostalCodeModel.js";
 import { matchedData } from "express-validator";
-import { sendSuccess } from "../utils/response.js";
+import { sendError, sendSuccess } from "../utils/response.js";
 
 class ContributionController {
   async createPostalCode(req, res) {
-    const { city, code, post } = matchedData(req);
+    try {
+      const userId = req.user.id;
+      const typeOfChange = "CREATE";
+      const { city, code, post } = matchedData(req);
 
-    const result = await pendingChangesPostalCodeModel.create({
-      city,
-      code,
-      post,
-    });
+      const result = await pendingChangesPostalCodeModel.create({
+        userId,
+        city,
+        code: Number(code),
+        post,
+        typeOfChange,
+      });
 
-    return sendSuccess(res, {
-      status: 201,
-      message: "New postal code row created.",
-      data: result,
-    });
+      return sendSuccess(res, {
+        status: 201,
+        message:
+          "New postal code suggested. Admin will review the suggestion and decide whether to accept it or not.",
+        data: result,
+      });
+    } catch (err) {
+      console.log(err);
+      sendError(res, {
+        status: 500,
+        message: "An error occurred while suggesting a new postal code.",
+      });
+    }
   }
 
   async editPostalCode(req, res) {
+    const userId = req.user.id;
+    const typeOfChange = "UPDATE";
     const { city, code, post } = matchedData(req);
 
-    const result = await pendingChangesPostalCodeModel.update(
-      { city, code },
-      { post },
-    );
+    const result = await pendingChangesPostalCodeModel.create({
+      userId,
+      code: Number(code),
+      post,
+      typeOfChange,
+      city,
+    });
 
     return sendSuccess(res, {
       status: 201,
-      message: "Postal code row edited.",
+      message:
+        "Postal code edit suggested. Admin will review the suggestion and decide whether to accept it or not.",
       data: result,
     });
   }
 
   async deletePostalCode(req, res) {
+    const userId = req.user.id;
+    const typeOfChange = "DELETE";
     const { code } = matchedData(req);
 
-    const result = await pendingChangesPostalCodeModel.delete({ code });
+    const result = await pendingChangesPostalCodeModel.create({
+      userId,
+      code: Number(code),
+      typeOfChange,
+    });
 
     return sendSuccess(res, {
-      message: "Postal code row deleted.",
+      message:
+        "Postal code deletion suggested. Admin will review the suggestion and decide whether to accept it or not.",
       data: result,
     });
   }
