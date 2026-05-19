05b7f9c04227d1fb99a38385dcb923d904d649fd
Goran Jovic <goran1010jovic@gmail.com>
2026-05-19 14:49:04 +0200

    feat: add React Helmet for dynamic page titles in PostalCodes component

--- frontend/src/components/PostalCodes/PostalCodes.jsx
+++ frontend/src/components/PostalCodes/PostalCodes.jsx
@@ -1,30 +1,38 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { SearchPostalCode } from "./SearchPostalCode";
 import { GetAllPostalCodes } from "./GetAllPostalCodes";
 import { PostalCodesResult } from "./PostalCodesResult";
+import { Helmet } from "react-helmet-async";
+import { LanguageContext } from "../../contextData/LanguageContext";
 
 function PostalCodes() {
   const [loading, setLoading] = useState(false);
   const [searchResult, setSearchResult] = useState([]);
+  const { t } = useContext(LanguageContext);
 
   return (
-    <div className="flex flex-col gap-8 flex-1 items-center w-full">
-      <section className="relative flex flex-col justify-center items-center gap-4 py-4">
-        <SearchPostalCode
-          setSearchResult={setSearchResult}
-          loading={loading}
-          setLoading={setLoading}
-        />
-        <GetAllPostalCodes
-          setSearchResult={setSearchResult}
-          loading={loading}
-          setLoading={setLoading}
-        />
-      </section>
-      <PostalCodesResult searchResult={searchResult} />
+    <>
+      <Helmet>
+        <title>{`${t("postal.title")} | ${t("appName")}`}</title>
+      </Helmet>
+      <div className="flex flex-col gap-8 flex-1 items-center w-full">
+        <section className="relative flex flex-col justify-center items-center gap-4 py-4">
+          <SearchPostalCode
+            setSearchResult={setSearchResult}
+            loading={loading}
+            setLoading={setLoading}
+          />
+          <GetAllPostalCodes
+            setSearchResult={setSearchResult}
+            loading={loading}
+            setLoading={setLoading}
+          />
+        </section>
+        <PostalCodesResult searchResult={searchResult} />
 
-      <div className="relative"></div>
-    </div>
+        <div className="relative"></div>
+      </div>
+    </>
   );
 }
 
