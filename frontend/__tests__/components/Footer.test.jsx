eef02986b6e68cbdfc4f500c484496e9f0187d4a
Goran Jovic <goran1010jovic@gmail.com>
2026-05-20 11:18:03 +0200

    test: fix failing tests in Footer.test.jsx

--- frontend/__tests__/components/Footer.test.jsx
+++ frontend/__tests__/components/Footer.test.jsx
@@ -1,10 +1,24 @@
 import { describe, test, expect } from "vitest";
 import { render, screen } from "@testing-library/react";
 import { Footer } from "../../src/components/Footer";
+import { LanguageContext } from "../../src/contextData/LanguageContext";
+import { useLanguage } from "../../src/customHooks/useLanguage";
+
+function MockLanguageProvider({ children }) {
+  const languageData = useLanguage();
+  return <LanguageContext value={languageData}>{children}</LanguageContext>;
+}
 
 describe("ErrorPage Component", () => {
   test("Footer component", () => {
-    render(<Footer />);
+    render(
+      <MockLanguageProvider>
+        <Footer />
+      </MockLanguageProvider>,
+    );
+
+    const name = screen.getByText(/goran jovi/i);
+    expect(name).toBeInTheDocument();
     const email = screen.getByText(/goran1010jovic@gmail.com/i);
     expect(email).toBeInTheDocument();
   });
