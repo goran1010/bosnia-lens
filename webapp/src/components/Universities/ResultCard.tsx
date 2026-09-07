dd7901f12c286181aa596b0acbb0dc680c0c4d65
Goran Jovic <goran1010jovic@gmail.com>
2026-09-07 16:50:53 +0200

    fix: consitent formatting

--- webapp/src/components/Universities/ResultCard.tsx
+++ webapp/src/components/Universities/ResultCard.tsx
@@ -2,8 +2,8 @@ import type { ReactNode } from "react";
 
 function ResultCard({ children }: { children: ReactNode }) {
   return (
-    <li className="border border-(--border-color) rounded-lg p-3 bg-(--surface-2) hover:bg-(--hover-surface) transition-colors">
-      {children}
+    <li className="border border-(--border-color) rounded-lg overflow-hidden bg-(--surface-2) hover:bg-(--hover-surface) transition-colors">
+      <div className="p-2 sm:p-4">{children}</div>
     </li>
   );
 }
